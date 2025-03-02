ba7a6683b8475cf1374c3266d8d1fc1c59fd4406
Luan Tenório <lluantenorio7@gmail.com>
2025-03-02 17:33:53 -0300

    fixing entity

--- internal/subject/entity/entity.go
+++ internal/subject/entity/entity.go
@@ -1,9 +1,10 @@
 package entity
 
-type SubjectEntity struct {
+type Subject struct {
 	Id        int    `json:"id"`
 	Name      string `json:"name"`
 	TotalTime int    `json:"total_time" db:"total_time"`
 	Avarage   int    `json:"avarage"`
 	CreatedAt string `json:"created_at" db:"created_at"`
+	UserId    int    `json:"-" db:"user_id"`
 }
