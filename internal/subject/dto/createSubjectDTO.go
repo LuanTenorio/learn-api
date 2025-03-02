860ea05786cc137acc98a1729cebe2caece71480
Luan Tenório <lluantenorio7@gmail.com>
2025-03-02 17:33:29 -0300

    Create dto

--- internal/subject/dto/createSubjectDTO.go
+++ internal/subject/dto/createSubjectDTO.go
@@ -1,5 +1,6 @@
 package dto
 
 type CreateSubjectDTO struct {
-	Name string `json:"name" validate:"required,min=3,max=150"`
+	Name   string `json:"name" validate:"required,min=3,max=150"`
+	UserId int    `json:"-" db:"user_id"`
 }
